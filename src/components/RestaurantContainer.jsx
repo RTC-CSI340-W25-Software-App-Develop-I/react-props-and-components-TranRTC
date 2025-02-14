2f4c6df5eacfa5b2ee7f5e9b81876ec8c167d062
TranRTC <tranquocbaosaigon@gmail.com>
2025-02-13 20:48:40 -0800

    working in class

---
+++ src/components/RestaurantContainer.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+
+const RestaurantContainer = () => {
+    return (
+        
+        <div className="RestaurantContainer">
+
+
+
+        </div>
+
+    );
+    
+};
+
+
+export default RestaurantContainer;
